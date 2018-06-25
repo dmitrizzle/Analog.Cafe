f6062ca915628048caf9e2120a8e63c0f735eb0a
Dmitri <d@eurekaking.com>
2018-06-25 16:45:57 +0700

    Fixed overwritten component

--- src/app/user/components/routes/Submit.js
+++ src/app/user/components/routes/Submit.js
@@ -1,8 +1,54 @@
-import { shallow } from "enzyme"
+import { Switch, Route } from "react-router-dom"
+import Loadable from "react-loadable"
 import React from "react"
 
-import Submit from "./Submit"
+import ArticleLoader from "../../../core/components/pages/Article/components/ArticleLoader"
 
-it("Render Submit routes without crashing", () => {
-  shallow(<Submit />)
+const Composer = Loadable({
+  loader: () => import("../pages/Composer"),
+  loading: ArticleLoader,
+  delay: 100
 })
+const RestoreComposerBackup = Loadable({
+  loader: () => import("../pages/RestoreComposerBackup"),
+  loading: ArticleLoader,
+  delay: 100
+})
+
+const NotFound = Loadable({
+  loader: () =>
+    import("../../../core/components/pages/Error/components/NotFound"),
+  loading: ArticleLoader,
+  delay: 100
+})
+const Rules = Loadable({
+  loader: () => import("../pages/Rules"),
+  loading: ArticleLoader,
+  delay: 100
+})
+const Submit = Loadable({
+  loader: () => import("../pages/Submit"),
+  loading: ArticleLoader,
+  delay: 100
+})
+const Upload = Loadable({
+  loader: () => import("../pages/Upload"),
+  loading: ArticleLoader,
+  delay: 100
+})
+
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/submit" component={Submit} />
+      <Route exact path="/submit/compose" component={Composer} />
+      <Route exact path="/submit/rules" component={Rules} />
+
+      <Route exact path="/submit/confirm-full-consent" component={Upload} />
+
+      <Route exact path="/submit/restore" component={RestoreComposerBackup} />
+
+      <Route path="/submit/*" component={NotFound} />
+    </Switch>
+  )
+}
