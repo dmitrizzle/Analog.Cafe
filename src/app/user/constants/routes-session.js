c5d2acb746f9de62c7855cb7db4abfc1c82893b9
Dmitri <d@eurekaking.com>
2018-10-03 22:30:45 -0700

    Add a tool to auto-refresh user login status

--- src/app/user/constants/routes-session.js
+++ src/app/user/constants/routes-session.js
@@ -3,6 +3,7 @@ import { HOST_API } from "../../constants"
 export const ROUTE_API_LOGIN_TWITTER = HOST_API + "/auth/twitter"
 export const ROUTE_API_LOGIN_FACEBOOK = HOST_API + "/auth/facebook"
 export const ROUTE_API_LOGIN_EMAIL = HOST_API + "/auth/email"
+export const ROUTE_API_LOGIN_REFRESH = HOST_API + "/auth/refresh"
 
 export const ROUTE_API_USER = HOST_API + "/auth/user"
 export const ROUTE_API_USER_PROFILE = HOST_API + "/users/me"
