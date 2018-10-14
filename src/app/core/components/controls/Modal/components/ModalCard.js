c71d1c698393bdd091782d607063773b8cd57f3f
Dmitri <d@eurekaking.com>
2018-10-13 23:08:05 -0700

    Auto-center modal

--- src/app/core/components/controls/Modal/components/ModalCard.js
+++ src/app/core/components/controls/Modal/components/ModalCard.js
@@ -2,13 +2,37 @@ import React from "react"
 
 import Card from "../../Card"
 
-export default props => {
-  return (
-    <Card
-      {...props}
-      style={{
-        margin: "5vh auto 90.1vh"
-      }}
-    />
-  )
+export const windowHeight = () =>
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight
+
+export default class extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      topOffset: 0
+    }
+  }
+  componentWillReceiveProps = () => {
+    const element = document.getElementById("modal-card")
+    if (element)
+      window.requestAnimationFrame(() => {
+        const topOffset = (windowHeight() - element.offsetHeight) / 2
+        this.setState({
+          topOffset: topOffset > 20 ? topOffset : 20
+        })
+      })
+  }
+  render = () => {
+    return (
+      <Card
+        {...this.props}
+        style={{
+          margin: `${this.state.topOffset}px auto 90.1vh`
+        }}
+        id="modal-card"
+      />
+    )
+  }
 }
