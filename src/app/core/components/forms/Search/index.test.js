d4326de44ff95d70d7b77276076715d3b1cd85fe
Dmitri <d@eurekaking.com>
2018-11-18 13:25:00 -0800

    Fix tests

--- src/app/core/components/forms/Search/index.test.js
+++ src/app/core/components/forms/Search/index.test.js
@@ -1,11 +1,13 @@
 import "jest-styled-components"
 
-import { shallow } from "enzyme"
+import { createMockStore } from "redux-test-utils"
+import { shallowWithStore } from "enzyme-redux"
 import React from "react"
 
 import Search from "./"
 
 it("Render Search, matches snapshot", () => {
-  const element = shallow(<Search />)
+  const store = createMockStore()
+  const element = shallowWithStore(<Search />, store)
   expect(element).toMatchSnapshot()
 })
