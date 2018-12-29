c853cb0b0eaa2080c05020c936a26a5247474f3d
Dmitri <d@eurekaking.com>
2018-12-29 13:13:46 -0800

    Fetch all author info with author-filtered lists

--- src/app/core/store/actions-list.js
+++ src/app/core/store/actions-list.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 import { CARD_ERRORS } from "../constants/messages-"
 import { HEADER_ERRORS } from "../../user/constants/messages-submission"
+import { ROUTE_API_AUTHORS } from "../constants/routes-article"
 import {
   ROUTE_API_LIST,
   ROUTE_API_LIST_SUBMISSIONS
@@ -20,10 +21,16 @@ export const setListPage = (page, appendItems) => {
       payload: page
     }
 }
-export function initListPage(state) {
+export const initListPage = state => {
   return {
     type: "LIST.INIT_PAGE",
     payload: state
+  }
+}
+export const setListAuthor = author => {
+  return {
+    type: "LIST.SET_AUTHOR",
+    payload: author
   }
 }
 
@@ -55,6 +62,20 @@ export const fetchListPage = (request, appendItems = false) => {
       )
     axios(makeAPIRequest(request))
       .then(response => {
+        const listAuthor =
+          (response.data &&
+            response.data.filter &&
+            response.data.filter.author &&
+            response.data.filter.author.id) ||
+          null
+        if (listAuthor) dispatch(fetchListAuthor(listAuthor))
+        else dispatch(setListAuthor(undefined))
+
+        console.log("add loading state for author")
+
+        if (request.url.includes(ROUTE_API_LIST_SUBMISSIONS))
+          dispatch(fetchListAuthor(getState().user.info.id))
+
         if (response.data.page["items-total"] > 0)
           dispatch(setListPage(response.data, appendItems))
         else if (request.url.includes(ROUTE_API_LIST_SUBMISSIONS)) {
@@ -71,6 +92,22 @@ export const fetchListPage = (request, appendItems = false) => {
             })
           )
         }
+      })
+      .catch(() => {
+        dispatch(
+          initListPage({
+            error: CARD_ERRORS.LIST
+          })
+        )
+      })
+  }
+}
+
+export const fetchListAuthor = id => {
+  return dispatch => {
+    axios(makeAPIRequest({ url: `${ROUTE_API_AUTHORS}/${id}` }))
+      .then(response => {
+        dispatch(setListAuthor(response.data.info))
       })
       .catch(() => {
         dispatch(
