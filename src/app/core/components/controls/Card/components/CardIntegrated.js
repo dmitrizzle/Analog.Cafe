d39a2ec6cf91f6cb39d2ad987be42d7779f5b0fb
Dmitri <d@eurekaking.com>
2018-12-28 19:32:07 -0800

    Fix styles

--- src/app/core/components/controls/Card/components/CardIntegrated.js
+++ src/app/core/components/controls/Card/components/CardIntegrated.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import CardPopup from "./CardPopup"
 
 export default styled(CardPopup)`
+  max-width: 100%;
   margin: ${props => props.theme.size.block.padding}em auto
     ${props => props.theme.size.block.spacing}em;
   box-shadow: 0 1px 1px
@@ -12,8 +13,12 @@ export default styled(CardPopup)`
   border-radius: ${props => props.theme.effects.borderRadius.small}em;
 
   ${props => props.theme.size.breakpoint.max.xs`
+    max-width: 100vw;
 		border-radius: 0;
     width: 100vw;
     margin-left: -${props => props.theme.size.block.padding}em;
+    & a {
+      margin-left: 0
+    }
 	`};
 `
