efe6c5edf0cdaf289390d4a8a226c200c1460fb5
Dmitri <d@eurekaking.com>
2019-02-19 01:19:47 -0800

    Adjust search defaults

--- src/app/core/constants/messages-search.js
+++ src/app/core/constants/messages-search.js
@@ -1,24 +1,24 @@
 export const SEARCH_RESULTS_FEATURED = [
   {
+    link: "/zine/a-beginner's-guide-to-film-photography-zq0f",
+    title: "A Beginner’s Guide to Film Photography",
+    snippet: "2019 Edition."
+  },
+  {
     link: "/resources",
     // image:
     //   "https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_520/image-froth_1402564_SklPuuHRNf.jpg",
     title: "Resources",
     snippet:
       "Film Grain Reference, downloads, essential guides, and select photo essays."
-  },
-  {
-    link: "/zine/a-beginner's-guide-to-film-photography-zq0f",
-    title: "A Beginner’s Guide to Film Photography",
-    snippet: "2019 Edition."
-  },
-  {
-    link: "/zine/35mm-film-price-guide-6zt1",
-    // image:
-    //   "https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_520/image-froth_750000_SkhbW7eNN.jpg",
-    title: "35mm Film Price Guide",
-    snippet: "Understand your photography costs."
   }
+  // {
+  //   link: "/zine/35mm-film-price-guide-6zt1",
+  //   // image:
+  //   //   "https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_520/image-froth_750000_SkhbW7eNN.jpg",
+  //   title: "35mm Film Price Guide",
+  //   snippet: "Understand your photography costs."
+  // }
   // {
   //   link: "/submit",
   //   // image:
