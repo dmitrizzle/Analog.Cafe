2a823e4cd1012e87657684fd052e5eb2173398e6
Dmitri <d@eurekaking.com>
2019-04-13 23:17:20 -0700

    Update picture

--- src/app/user/components/pages/MustReads/constants.js
+++ src/app/user/components/pages/MustReads/constants.js
@@ -79,7 +79,7 @@ export const MUST_READS_CONTENT = {
     },
     {
       title: "Film Photography Costs and Prices",
-      poster: "image-froth_1333333_rkLFrJrw4",
+      poster: "image-froth_1507950_HyGXTrg9E",
       to: "/zine/film-photography-costs-and-prices-kd5j"
     },
     {
